cd17077fe14f038fdf108de2b2bf1b2d5cb5ed14
Alex6n <hheikal.work@gmail.com>
2023-10-20 09:48:11 +0300

    interactive tab styling

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -12,7 +12,7 @@ export default function Tabs() {
     <div className="mt-5 rounded-xl bg-azure-radiance-100 p-1 max-w-fit">
       <Tab.List className="flex gap-1 text-sm font-bold">
         <a href="#" className="special-tab"><GrFormAdd />New task</a>
-        <Tab className="active-tab"><TbClockHour2 className="mr-1"/>Today's tasks</Tab>
+        <Tab className="tab"><TbClockHour2 className="mr-1"/>Today's tasks</Tab>
         <Tab className="tab"><BsCalendar4Week className="mr-1"/>Next 7 Days</Tab>
         <Tab className="tab"><MdClearAll className="mr-1"/>All</Tab>
         <Tab className="tab"><VscCheckAll className="mr-1" />Completed</Tab>
--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,8 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@headlessui/tailwindcss')({ prefix: 'ui' })
+  ],
 }
 
