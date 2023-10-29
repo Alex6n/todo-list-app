d3e11f285ff54d06042e7fca5f6ae20e16bc4fc1
Alex6n <hheikal.work@gmail.com>
2023-10-30 00:56:25 +0200

    Update Tasks.js

--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -16,14 +16,16 @@ export default function Tasks({ task, catogery }) {
       
       <div className="grid grid-cols-4 gap-3">
 
-        <div className="card-devider duration-300">
-          <a className="absolute text-6xl transition-opacity hover:opacity-100 opacity-0 p-12 rounded-xl">
-            <AiOutlinePlusCircle/>
-          </a>
-          <img className="h-40 pr-1 rounded-xl object-cover justify-self-center transition-opacity duration-300 hover:opacity-30"
-            src={require(`../../assets/${task.Icon}`)}
-            alt="Task Icon"
-          />
+        <div className="card-devider relative flex flex-col items-center">
+          <div className="group flex flex-col items-center justify-center">
+            <a className="absolute text-6xl text-gray-800 transition-opacity group-hover:opacity-100 opacity-0 duration-500">
+              <AiOutlinePlusCircle/>
+            </a>
+            <img className="h-40 pr-1 rounded-xl object-cover justify-self-center transition-opacity duration-300 group-hover:opacity-30"
+              src={require(`../../assets/${task.Icon}`)}
+              alt="Task Icon"
+              />
+          </div>
         </div>
         
         <div className="col-span-2 max-w-fit card-devider w-96">
@@ -31,28 +33,26 @@ export default function Tasks({ task, catogery }) {
             <input onChange={(e) => dispatch(instantTitle(e.target.value))} type="text"
               className="font-extrabold text-lg bg-transparent outline-none ring-0" value={edit.Title} />
             <div className="overflow-scroll h-28 max-w-xl">
-              <textarea onChange={(e) => dispatch(instantDesc(e.target.value))} rows="10"
+              <textarea onChange={(e) => dispatch(instantDesc(e.target.value))} rows="5"
                 className="font-normal text-sm whitespace-pre-wrap bg-transparent outline-none ring-0 w-full" value={edit.Describtion} />
             </div>
           </div>
-          <div>
-            <div className="font-normal flex justify-between text-xs h-fit w-96">
+            <div className="font-normal flex justify-between text-xs h-fit w-96 pr-3">
               <p>Expected By:
                 <input type="date" onChange={(e) => dispatch(instantExpectedBy(e.target.value))} value={edit.ExpectedBy}
                   min={new Date().toISOString().slice(0, 10)} className="bg-sky-50/0" /></p>
               <p>Created On: {task.CreatedOn}</p>
             </div>
-          </div>
         </div>
 
-        <div>
-          <div className="flex justify-center mb-3">
+        <div className='flex flex-col items-center'>
+          <div className="mb-5">
             <div className="mt-3 flex gap-1 text-sm font-bold">
               <a href="#" onClick={() => dispatch(instantPriority())} className={`filter badge ${edit.Priority.toLowerCase()}`} >{edit.Priority}</a>
             </div>
           </div>
 
-          <div>
+          <div className='flex flex-col justify-center'>
             <a href="#" className="card-action cmplt"><ImCheckmark className="mr-1" /> Apply Changes</a>
           </div>
 
